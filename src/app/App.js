b58f71f1f9a991766b4299003b0152b73bf56a6d
Luke Walsh <lukewalsh@lwalsh-mbpr19.lan1>
2020-09-01 19:14:25 -0700

    Restructure folder system to divide css/js by model

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from './Table'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Pickup Events</header>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/table/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [],
+    };
+  }
+}
+
+export default Table;
