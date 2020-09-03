119c4721c7ab30b5d517864223ec24385843507b
Luke Walsh <lhw2096@yahoo.com>
2020-09-02 22:17:29 -0700

    WIP commit on creating event form
    Co-authored-by: Kevin De La Torre <kevindelatorre@live.com>

--- src/Add/form.js
+++ src/Add/form.js
@@ -1,42 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './form.css'
 
-const Form = ({ setShowAddButtonTrue }) =>
-  <div className="add-form">
-    <form>
-      <label class="form-add-item">
-        Event title:
-        <input type="text" name="name" />
-      </label>
-      <label class="form-add-item">
-        Description:
-        <input type="textarea" name="description" />
-      </label>
-      <label class="form-add-item">
-        People:
-        <input type="text" name="people" />
-      </label>
-      <label class="form-add-item">
-        Location:
-        <input type="text" name="location"/>
-      </label>
-      {/* TODO: Install datepicker package */}
-      <label class="form-add-item">
-        Date ( 2020/09/02 YEAR/MONTH/DAY ):
-        <input type="text" name="date" />
-      </label>
-      <label class="form-add-item">
-        Start Time:
-      </label>
-      <label class="form-add-item">
-        End Time:
-      </label>
-      <label class="form-add-item">
-        Category:
-        <input type="text" name="category" />
-      </label>
-    </form>
-    <button onClick={setShowAddButtonTrue}>Submit</button>
-  </div>
+const BASE_URL = "https://pickup-app-backend.herokuapp.com/api/";
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+
+    this.createEvent = this.createEvent.bind(this)
+  }
+
+  createEvent(event) {
+    this.props.setShowAddButtonTrue();
+    const data = new FormData(event.target);
+    // return (axios.post(`${BASE_URL}events?title=${event.title}&description=${event.description}`))
+  }
+
+  render() {
+    return(
+      <div className="form-box">
+        <h1>Add Event</h1>
+        <form className="add-form" onSubmit={this.createEvent}>
+          <label htmlFor="title" className="form-add-item">
+            Event title:
+            <input type="text" id="name" name="name" />
+          </label>
+          <label htmlFor="description" className="form-add-item">
+            Description:
+            <input type="textarea" id="description" name="description" />
+          </label>
+          <label className="form-add-item">
+            People:
+            <input type="text" name="people" />
+          </label>
+          <label className="form-add-item">
+            Location:
+            <input type="text" name="location"/>
+          </label>
+          {/* TODO: Install datepicker package */}
+          <label className="form-add-item">
+            Date ( yyyy/mm/dd ):
+            <input type="text" name="date" />
+          </label>
+          <label className="form-add-item">
+            Start Time:
+            <input type="text" name="start-time" />
+          </label>
+          <label className="form-add-item">
+            End Time:
+            <input type="text" name="end-time" />
+          </label>
+          <label className="form-add-item">
+            Category:
+            <input type="text" name="category" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+}
 
 export default Form;
