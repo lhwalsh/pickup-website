e7436a3214d45da8c3dd2740fcdcc1e432f4c214
Luke Walsh <lhw2096@yahoo.com>
2020-09-05 09:41:54 -0700

    Control form elements by using React component state

--- src/Add/form.js
+++ src/Add/form.js
@@ -8,13 +8,38 @@ class Form extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      title: '',
+      description: '',
+      people: '',
+      location: '',
+      date: '',
+      start_time: '',
+      end_time: '',
+      category: '',
+    };
+
     this.createEvent = this.createEvent.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  createEvent(event) {
+  createEvent() {
+    const {
+      title,
+      description,
+      people,
+      location,
+      date,
+      start_time,
+      end_time,
+      category
+    } = this.state;
     this.props.setShowAddButtonTrue();
-    const data = new FormData(event.target);
-    // return (axios.post(`${BASE_URL}events?title=${event.title}&description=${event.description}`))
+    return (axios.post(`${BASE_URL}events?title=${title}&description=${description}&people=${people}&location=${location}&date=${date}&start_time=${start_time}&end_time=${end_time}&category=${category}`))
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
@@ -23,39 +48,39 @@ class Form extends Component {
         <h1>Add Event</h1>
         <form className="add-form" onSubmit={this.createEvent}>
           <label htmlFor="title" className="form-add-item">
-            Event title:
-            <input type="text" id="name" name="name" />
+            Title:
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
           </label>
           <label htmlFor="description" className="form-add-item">
             Description:
-            <input type="textarea" id="description" name="description" />
+            <input type="textarea" name="description" value={this.state.description} onChange={this.handleChange} />
           </label>
           <label className="form-add-item">
             People:
-            <input type="text" name="people" />
+            <input type="text" name="people" value={this.state.people} onChange={this.handleChange} />
           </label>
           <label className="form-add-item">
             Location:
-            <input type="text" name="location"/>
+            <input type="text" name="location" value={this.state.location} onChange={this.handleChange} />
           </label>
           {/* TODO: Install datepicker package */}
           <label className="form-add-item">
-            Date ( yyyy/mm/dd ):
-            <input type="text" name="date" />
+            Date:
+            <input type="text" name="date" value={this.state.date} onChange={this.handleChange} />
           </label>
           <label className="form-add-item">
             Start Time:
-            <input type="text" name="start-time" />
+            <input type="text" name="start_time" value={this.state.start_time} onChange={this.handleChange} />
           </label>
           <label className="form-add-item">
             End Time:
-            <input type="text" name="end-time" />
+            <input type="text" name="end_time" value={this.state.end_time} onChange={this.handleChange} />
           </label>
           <label className="form-add-item">
             Category:
-            <input type="text" name="category" />
+            <input type="text" name="category" value={this.state.category} onChange={this.handleChange} />
           </label>
-          <input type="submit" value="Submit" />
+          <button type="submit">Submit</button>
         </form>
       </div>
     )
