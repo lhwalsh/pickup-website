c0095e1cab51ff56a2d5d459859b2a11ea4def82
Luke Walsh <lhw2096@yahoo.com>
2020-09-13 22:59:16 -0700

    Remove unnecessary class
    Co-authored-by: Kevin De La Torre <kevindelatorre@live.com>

--- src/Event/event-expanded.js
+++ src/Event/event-expanded.js
@@ -8,25 +8,25 @@ const EventExpanded = ({ event, setShowPreviewTrue }) =>
     <h1 className="event-item">
       {event.title}
     </h1>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.description}
     </span>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.people}
     </span>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.location}
     </span>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.date}
     </span>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.start_time}
     </span>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.end_time}
     </span>
-    <span style={{ fontSize: 25 }} className="item">
+    <span style={{ fontSize: 25 }}>
       {event.category}
     </span>
   </div>
