8dbde084267052b130c5e194ba15034b5a611d80
Luke Walsh <lhw2096@yahoo.com>
2020-11-14 13:38:46 -0800

    Move header folder logic back into main app
    Co-authored-by: Kevin De La Torre <kevindelatorre@live.com>

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Header from '../Header'
-import Table from '../Table'
-import "fontsource-baloo-tammudu-2/500.css"
+import Table from '../Table';
+import { IoIosAddCircleOutline } from 'react-icons/io';
+import "fontsource-baloo-tammudu-2/500.css";
 import './index.css';
 
 function App () {
   return (
     <div className="App">
-      <Header />
+      <div className="Header">
+        <span className="home">Pickup</span>
+        <div className="add-icon">
+          <IoIosAddCircleOutline size={70} color="white" />
+        </div>
+      </div>
       <Table />
     </div>
   );
